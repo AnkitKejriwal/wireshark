AnkitKejriwal/wireshark

commit fbddfcfc869eb181abf3a09d5b257497c1be969d

From Hitoshi Irino: The function "dissect_v9_pdu" of "epan/dissectors/packet-netflow.c" decodes NetFlow v9 packets and IPFIX packets with same logic. But, the "scope field" is different between NetFlow v9 and IPFIX. NetFlow v9 has only 5 kind of scopes. On the other hand, many Information Elements can be used as scope fields in IPFIX packets.
git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@32627 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-netflow.c
@@ -588,6 +588,10 @@ static int	dissect_v9_data(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdu
 				int offset, guint16 id, guint length, hdrinfo_t * hdrinfo);
 static guint	dissect_v9_pdu(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree,
 			       int offset, struct v9_template * tplt, hdrinfo_t * hdrinfo);
+static guint	dissect_v9_pdu_scope(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree,
+			       int offset, struct v9_template * tplt);
+static guint	dissect_v9_pdu_data(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree,
+				    int offset, struct v9_template * tplt, hdrinfo_t * hdrinfo, guint8 ipfix_scope_flag);
 static int	dissect_v9_options_template(tvbuff_t *tvb, packet_info *pinfo, proto_tree *pdutree,
 				   int offset, hdrinfo_t *hdrinfo, guint16 flowset_id);
 static int	dissect_v9_template(proto_tree * pdutree, tvbuff_t * tvb, packet_info *pinfo,
@@ -1352,7 +1356,75 @@ static guint
 dissect_v9_pdu(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, int offset,
     struct v9_template * tplt, hdrinfo_t * hdrinfo)
 {
-        int             orig_offset = offset;
+	int             orig_offset = offset;
+	if (hdrinfo->vspec == 10) {
+		offset += dissect_v9_pdu_data(tvb, pinfo, pdutree, offset, tplt, hdrinfo, 1);
+	} else  {
+		offset += dissect_v9_pdu_scope(tvb, pinfo, pdutree, offset, tplt);
+	}
+	offset += dissect_v9_pdu_data(tvb, pinfo, pdutree, offset, tplt, hdrinfo, 0);
+	return (guint) (offset - orig_offset);
+}
+
+static guint
+dissect_v9_pdu_scope(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, int offset,
+    struct v9_template * tplt)
+{
+	int             orig_offset = offset;
+	proto_item *    ti;
+	if(tplt->scopes != NULL) {
+		int             i;
+		for(i = 0; i < tplt->count_scopes; i++) {
+			guint16 type = tplt->scopes[i].type;
+			guint16 length = tplt->scopes[i].length;
+			if (!length) {
+				continue;
+			}
+			switch( type ) {
+			case 1: /* system */
+				ti = proto_tree_add_item(pdutree, hf_cflow_scope_system,
+				       tvb, offset, length, FALSE);
+				if (length > 0 && length != 4) {
+					expert_add_info_format(pinfo, ti, PI_MALFORMED, PI_WARN,
+							       "ScopeSystem: invalid size %u", length);
+				} /* zero-length system scope is valid */
+				break;
+			case 2: /* interface */
+				ti = proto_tree_add_item(pdutree, hf_cflow_scope_interface,
+				       tvb, offset, length, FALSE);
+				if (length != 4) {
+					expert_add_info_format(pinfo, ti, PI_MALFORMED, PI_WARN,
+							       "ScopeInterface: invalid size %u", length);
+				}
+				break;
+			case 3: /* linecard */
+				proto_tree_add_item(pdutree, hf_cflow_scope_linecard,
+						tvb, offset, length, FALSE);
+				break;
+			case 4: /* netflow cache */
+				proto_tree_add_item(pdutree, hf_cflow_scope_cache,
+						tvb, offset, length, FALSE);
+				break;
+			case 5: /* tplt */
+				proto_tree_add_item(pdutree, hf_cflow_scope_template,
+						tvb, offset, length, FALSE);
+				break;
+			default: /* unknown */
+				proto_tree_add_item(pdutree, hf_cflow_scope_unknown,
+						tvb, offset, length, FALSE);
+				break;
+			}
+			offset += length;
+		}
+	}
+        return (guint) (offset - orig_offset);
+}
+
+static guint
+dissect_v9_pdu_data(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, int offset,
+		    struct v9_template * tplt, hdrinfo_t * hdrinfo, guint8 ipfix_scope_flag)
+{
+	int             orig_offset = offset;
 	int             i;
 	int             rev;
 	nstime_t        ts_start[2], ts_end[2];
@@ -1363,75 +1435,36 @@ dissect_v9_pdu(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, int of
 	guint32         msec_delta;
 	proto_tree *    timetree = 0;
 	proto_item *    timeitem = 0;
-        address         local_addr, remote_addr;
-        guint16         local_port = 0, remote_port = 0, ipv4_id = 0, icmp_id = 0;
-        guint32         uid = 0, pid = 0;
-        int             uname_len;
-        gchar *         uname_str = NULL;
-        int             cmd_len;
-        gchar *         cmd_str = NULL;
-        guint16         got_flags = 0;
+	address         local_addr, remote_addr;
+	guint16         local_port = 0, remote_port = 0, ipv4_id = 0, icmp_id = 0;
+	guint32         uid = 0, pid = 0;
+	int             uname_len;
+	gchar *         uname_str = NULL;
+	int             cmd_len;
+	gchar *         cmd_str = NULL;
+	guint16         got_flags = 0;
 	proto_item *    ti;
 	const guint8 *reftime;
+	guint16 count = ipfix_scope_flag ? tplt->count_scopes : tplt->count;
+	struct v9_template_entry *entries = ipfix_scope_flag ? tplt->scopes : tplt->entries;
+	
+	if (entries == NULL) {
+		return 0;
+	}
 
 	offset_s[0] = offset_s[1] = offset_e[0] = offset_e[1] = 0;
 	msec_start[0] = msec_start[1] = msec_end[0] = msec_end[1] = 0;
 
-	if(tplt->scopes != NULL) {
-		for(i = 0; i < tplt->count_scopes; i++) {
-			guint16 type = tplt->scopes[i].type;
-	    		guint16 length = tplt->scopes[i].length;
-			if (!length) {
-				continue;
-			}
-	   		switch( type ) {
-	   		case 1: /* system */
-				ti = proto_tree_add_item(pdutree, hf_cflow_scope_system,
-				       tvb, offset, length, FALSE);
-				if (length > 0 && length != 4) {
-					expert_add_info_format(pinfo, ti, PI_MALFORMED, PI_WARN,
-							       "ScopeSystem: invalid size %u", length);
-				} /* zero-length system scope is valid */
-				break;
-	   		case 2: /* interface */
-				ti = proto_tree_add_item(pdutree, hf_cflow_scope_interface,
-				       tvb, offset, length, FALSE);
-				if (length != 4) {
-					expert_add_info_format(pinfo, ti, PI_MALFORMED, PI_WARN,
-							       "ScopeInterface: invalid size %u", length);
-				}
-				break;
-	   		case 3: /* linecard */
-				proto_tree_add_item(pdutree, hf_cflow_scope_linecard,
-						tvb, offset, length, FALSE);
-				break;
-	   		case 4: /* netflow cache */
-				proto_tree_add_item(pdutree, hf_cflow_scope_cache,
-						tvb, offset, length, FALSE);
-				break;
-	   		case 5: /* tplt */
-				proto_tree_add_item(pdutree, hf_cflow_scope_template,
-						tvb, offset, length, FALSE);
-				break;
-	   		default: /* unknown */
-				proto_tree_add_item(pdutree, hf_cflow_scope_unknown,
-						tvb, offset, length, FALSE);
-				break;
-			}
-			offset += length;
- 		}
-	}
-
-	for (i = 0; i < tplt->count; i++) {
-                guint64 pen_type;
+	for (i = 0; i < count; i++) {
+		guint64 pen_type;
 		guint16 type, length;
 		guint32 pen = 0;
 
 		rev = 0;
-		type = tplt->entries[i].type;
-		length = tplt->entries[i].length;
+		type = entries[i].type;
+		length = entries[i].length;
 		if (hdrinfo->vspec == 10 && type & 0x8000) {
-                  pen = tplt->entries[i].pen;
+		  pen = entries[i].pen;
 		  if (pen == REVPEN) { /* reverse PEN */
 		    type &= 0x7fff;
 		    rev = 1;
@@ -1455,7 +1488,7 @@ dissect_v9_pdu(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, int of
 				    tvb, offset, length,
 				    "Octets: length %u", length);
 			}
-		  break;
+			break;
 
 		case 86: /* PACKETS_PERMANENT */
 		case 2: /* packets */

commit 0d64ecb4191d5d74f4e884aacc05ba041aadf17c

From Hitoshi Irino via bug 4942: 1. fix the bug in dissect_v9_pdu. (The bug is introduced in r32627, It's my fault, I'm sorry.) When option data record is decoded, unpatched dissect_v9_pdu decode only scope fields, it does not decode following data fields. And it runs in endless loop when length of a scope filed is 0. This patch solve these problem. 2. defines some value_strings for some fields. 3. updates URLs in comment.
git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@33348 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-netflow.c
@@ -35,6 +35,7 @@
  ** See
  **
  ** http://www.cisco.com/warp/public/cc/pd/iosw/prodlit/tflow_wp.htm
+ ** http://www.cisco.com/en/US/technologies/tk648/tk362/technologies_white_paper09186a00800a3db9.html
  **
  ** Cisco ASA5500 Series
  ** http://www.cisco.com/en/US/docs/security/asa/asa83/netflow/netflow.html
@@ -45,6 +46,7 @@
  ** http://www.ietf.org/rfc/rfc5102.txt
  ** http://www.ietf.org/rfc/rfc5103.txt
  ** http://www.iana.org/assignments/ipfix/ipfix.xml
+ ** http://www.iana.org/assignments/psamp-parameters/psamp-parameters.xml
  ** for IPFIX
  **
  *****************************************************************************
@@ -60,7 +62,7 @@
  **
  ** See also
  **
- ** http://www.cisco.com/univercd/cc/td/doc/cisintwk/intsolns/netflsol/nfwhite.htm
+ ** http://www.cisco.com/en/US/docs/ios/solutions_docs/netflow/nfwhite.html
  **
  ** $Yahoo: //depot/fumerola/packet-netflow/packet-netflow.c#14 $
  */
@@ -201,6 +203,7 @@ static int      ett_flowtime = -1;
 static int      ett_template = -1;
 static int      ett_field = -1;
 static int      ett_dataflowset = -1;
+static int      ett_fwdstat = -1;
 
 /*
  * cflow header
@@ -326,6 +329,7 @@ static int      hf_cflow_direction = -1;
 static int      hf_cflow_if_name = -1;
 static int      hf_cflow_if_descr = -1;
 static int      hf_cflow_sampler_name = -1;
+static int      hf_cflow_forwarding_status_code = -1;
 static int      hf_cflow_forwarding_status = -1;
 static int      hf_cflow_forwarding_code = -1;
 static int      hf_cflow_nbar_appl_desc = -1;
@@ -1386,12 +1390,14 @@ dissect_v9_pdu(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, int of
 {
 	int             orig_offset = offset;
 
-	if (tplt->option_template && (hdrinfo->vspec == 9)) {
-		offset += dissect_v9_pdu_scope(tvb, pinfo, pdutree, offset, tplt);
-
-	} else {
-		offset += dissect_v9_pdu_data(tvb, pinfo, pdutree, offset, tplt, hdrinfo, tplt->option_template);
+	if (tplt->scopes != NULL && tplt->count_scopes > 0) {
+		if (hdrinfo->vspec == 9) {
+			offset += dissect_v9_pdu_scope(tvb, pinfo, pdutree, offset, tplt);
+		} else if (hdrinfo->vspec == 10) {
+			offset += dissect_v9_pdu_data(tvb, pinfo, pdutree, offset, tplt, hdrinfo, 1);
+		}
 	}
+	offset += dissect_v9_pdu_data(tvb, pinfo, pdutree, offset, tplt, hdrinfo, 0);
 
 	return (guint) (offset - orig_offset);
 }
@@ -1481,7 +1487,8 @@ dissect_v9_pdu_data(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, i
 	const guint8 *reftime;
 	guint16 count = ipfix_scope_flag ? tplt->count_scopes : tplt->count;
 	struct v9_template_entry *entries = ipfix_scope_flag ? tplt->scopes : tplt->entries;
-	
+	proto_tree *    fwdstattree = 0;	
+
 	if (entries == NULL) {
 		/* I don't think we can actually hit this condition.
 		   If we can, what would cause it?  Does this need a
@@ -2127,9 +2134,15 @@ dissect_v9_pdu_data(tvbuff_t * tvb, packet_info * pinfo, proto_tree * pdutree, i
 			break;
 
 		case 89: /* FORWARDING_STATUS */
-			proto_tree_add_item(pdutree, hf_cflow_forwarding_status,
+			/* Forwarding status is encoded on 1 byte with
+			 * the 2 left bits giving the status and the 6
+			 * remaining bits giving the reason code. */
+			ti = proto_tree_add_item(pdutree, hf_cflow_forwarding_status_code,
 			    tvb, offset, length, FALSE);
-			proto_tree_add_item(pdutree, hf_cflow_forwarding_code,
+			fwdstattree = proto_item_add_subtree(ti, ett_fwdstat);
+			proto_tree_add_item(fwdstattree, hf_cflow_forwarding_status,
+ 			    tvb, offset, length, FALSE);
+			proto_tree_add_item(fwdstattree, hf_cflow_forwarding_code,
 			    tvb, offset, length, FALSE);
 			break;
 
@@ -4042,6 +4055,40 @@ static const value_string v9_extended_firewall_event[] = {
 	{ 1004, "Flow denied (TCP flow beginning with not TCP SYN)"},	
 	{ 0, NULL }
 };
+static const value_string engine_type[] = {
+	{ 0, "RP"},
+	{ 1, "VIP/Linecard"},
+	{ 2, "PFC/DFC" },
+	{ 0, NULL }
+};
+static const value_string v9_flow_end_reason[] = {
+	{ 0, "Unknown"},
+	{ 1, "Idle timeout"},
+	{ 2, "Active timeout" },
+	{ 3, "End of Flow detected" },
+	{ 4, "Forced end" },
+	{ 5, "Lack of resources" },	
+	{ 0, NULL }
+};
+static const value_string v9_biflow_direction[] = {
+	{ 0, "Arbitrary"},
+	{ 1, "Initiator"},
+	{ 2, "ReverseInitiator" },
+	{ 3, "Perimeter" },
+	{ 0, NULL }
+};
+static const value_string selector_algorithm[] = {
+	{ 0, "Reserved"},
+	{ 1, "Systematic count-based Sampling"},
+	{ 2, "Systematic time-based Sampling"},
+	{ 3, "Random n-out-of-N Sampling"},
+	{ 4, "Uniform probabilistic Sampling"},
+	{ 5, "Property match Filtering"},
+	{ 6, "Hash based Filtering using BOB"},
+	{ 7, " Hash based Filtering using IPSX"},
+	{ 8, "Hash based Filtering using CRC"},	
+	{ 0, NULL }
+};
 
 
 static int
@@ -4303,7 +4350,7 @@ proto_register_netflow(void)
 		 },
 		{&hf_cflow_engine_type,
 		 {"EngineType", "cflow.engine_type",
-		  FT_UINT8, BASE_DEC, NULL, 0x0,
+		  FT_UINT8, BASE_DEC, VALS(engine_type), 0x0,
 		  "Flow switching engine type", HFILL}
 		 },
 		{&hf_cflow_engine_id,
@@ -5006,7 +5053,7 @@ proto_register_netflow(void)
 		},
 		{&hf_cflow_flow_end_reason,
 		 {"Flow End Reason", "cflow.flow_end_reason",
-		  FT_UINT8, BASE_DEC, NULL, 0x0,
+		  FT_UINT8, BASE_DEC, VALS(v9_flow_end_reason), 0x0,
 		  NULL, HFILL}
 		},
 		{&hf_cflow_common_properties_id,
@@ -5386,7 +5433,7 @@ proto_register_netflow(void)
 		},
 		{&hf_cflow_biflow_direction,
 		 {"Biflow Direction", "cflow.biflow_direction",
-		  FT_UINT8, BASE_DEC, NULL, 0x0,
+		  FT_UINT8, BASE_DEC, VALS(v9_biflow_direction), 0x0,
 		  NULL, HFILL}
 		},
 		{&hf_cflow_ethernet_header_length,
@@ -5586,7 +5633,7 @@ proto_register_netflow(void)
 		},
 		{&hf_cflow_selector_algorithm,
 		 {"Selector Algorithm", "cflow.selector_algorithm",
-		  FT_UINT16, BASE_DEC, NULL, 0x0,
+		  FT_UINT16, BASE_DEC, VALS(selector_algorithm), 0x0,
 		  NULL, HFILL}
 		},
 		{&hf_cflow_sampling_packet_interval,
@@ -5952,7 +5999,8 @@ proto_register_netflow(void)
 		&ett_flowtime,
 		&ett_template,
 		&ett_field,
-		&ett_dataflowset
+		&ett_dataflowset,
+		&ett_fwdstat
 	};
 
 	module_t *netflow_module;