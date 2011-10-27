AnkitKejriwal/wireshark

commit c27e3a44ba8b2c5fda1783d1804864dc2c248461

Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6491 :
Don't set mr_mult_req_info->num_services until we're sure we've
initialized the full structure for that service.  Otherwise if we
happen to throw an exception before initializing the whole
structure, we'll core someplace (like dissect_cip_generic_service_rsp())
which expects all num_services entries to be fully initialized.



git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@39626 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-cip.c
@@ -3192,7 +3192,6 @@ dissect_cip_multiple_service_packet_req(tvbuff_t *tvb, packet_info *pinfo, proto
       {
          mr_mult_req_info = se_alloc(sizeof(mr_mult_req_info_t));
          mr_mult_req_info->service = SC_MULT_SERV_PACK;
-         mr_mult_req_info->num_services = num_services;
          mr_mult_req_info->requests = se_alloc(sizeof(cip_req_info_t)*num_services);
          cip_req_info->pData = mr_mult_req_info;
       }
@@ -3254,6 +3253,15 @@ dissect_cip_multiple_service_packet_req(tvbuff_t *tvb, packet_info *pinfo, proto
       {
          dissect_cip_data(mult_serv_tree, next_tvb, 0, pinfo, NULL );
       }
+
+      /* Don't set mr_mult_req_info->num_services until we're sure we've
+       * initialized the full structure for that service.  Otherwise if we
+       * happen to throw an exception here (before initializing the whole
+       * structure), we'll core someplace (like
+       * dissect_cip_generic_service_rsp()) which expects all num_services
+       * entries to be fully initialized.
+       */
+      mr_mult_req_info->num_services = i;
    }
 
 }

commit 27ec2f9ffba927070565a0a1d82145852978b032

Correct an off-by-one error introduced in r39626.
git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@39631 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-cip.c
@@ -3263,7 +3263,7 @@ dissect_cip_multiple_service_packet_req(tvbuff_t *tvb, packet_info *pinfo, proto
        * dissect_cip_generic_service_rsp()) which expects all num_services
        * entries to be fully initialized.
        */
-      mr_mult_req_info->num_services = i;
+      mr_mult_req_info->num_services = i+1;
    }
 
 }