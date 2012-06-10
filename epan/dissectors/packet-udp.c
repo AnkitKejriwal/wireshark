AnkitKejriwal/wireshark

commit e9e53dc60eaca923f990244a245e72784d65b87c

Use length field from UDP header as the pseudo-header's UDP length field instead of using the reported_len.  Fixes https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6711
git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@40387 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-udp.c
@@ -462,12 +462,12 @@ dissect(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 ip_proto)
       switch (pinfo->src.type) {
 
       case AT_IPv4:
-        phdr[0] = g_htonl((ip_proto<<16) + reported_len);
+        phdr[0] = g_htonl((ip_proto<<16) | udph->uh_ulen);
         cksum_vec[2].len = 4;
         break;
 
       case AT_IPv6:
-        phdr[0] = g_htonl(reported_len);
+        phdr[0] = g_htonl(udph->uh_ulen);
         phdr[1] = g_htonl(ip_proto);
         cksum_vec[2].len = 8;
         break;

commit 0ca74681cd08accef2e0f4702aee022ee9f060f5

Fix UDP-Lite checksum verification bug introduced with r40387 and reported by "answer" via http://ask.wireshark.org/questions/11798/is-udp-lite-checksum-correct-ipv6 There is a difference between the UDP and UDP-Lite pseudo header that wasn't realized when r40387 was committed.  Details of the difference in http://tools.ietf.org/html/rfc3828#section-3.2
git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@43187 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-udp.c
@@ -462,12 +462,18 @@ dissect(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 ip_proto)
       switch (pinfo->src.type) {
 
       case AT_IPv4:
-        phdr[0] = g_htonl((ip_proto<<16) | udph->uh_ulen);
+        if (ip_proto == IP_PROTO_UDP)
+          phdr[0] = g_htonl((ip_proto<<16) | udph->uh_ulen);
+	else
+          phdr[0] = g_htonl((ip_proto<<16) | reported_len);
         cksum_vec[2].len = 4;
         break;
 
       case AT_IPv6:
-        phdr[0] = g_htonl(udph->uh_ulen);
+        if (ip_proto == IP_PROTO_UDP)
+          phdr[0] = g_htonl(udph->uh_ulen);
+	else
+          phdr[0] = g_htonl(reported_len);
         phdr[1] = g_htonl(ip_proto);
         cksum_vec[2].len = 8;
         break;