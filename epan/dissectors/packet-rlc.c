AnkitKejriwal/wireshark

commit 5eab3a6902b1b106e4eb6c8a5ed25814e99879c4

From Jacob Nordgren and Rishie Sharma: - umts_fp: fixed so CS content is not malformed content - RLC: added channel tree so you can filter on rbid etc - umts_fp,rlc,mac: added some hdsch-common code
git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@44292 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-rlc.c
@@ -68,6 +68,14 @@ static gboolean global_rlc_ciphered = FALSE;
 /* Stop trying to do reassembly if this is true. */
 static gboolean fail = FALSE;
 
+/* LI size preference */
+static gint global_rlc_li_size = RLC_LI_UPPERLAYER;
+static const enum_val_t li_size_enumvals[] = {
+	{"7 bits", "7 bits", RLC_LI_7BITS},
+	{"15 bits", "15 bits", RLC_LI_15BITS},
+	{"Let upper layers decide", "Let upper layers decide", RLC_LI_UPPERLAYER},
+	{NULL, NULL, -1}};
+
 /* fields */
 static int hf_rlc_seq = -1;
 static int hf_rlc_ext = -1;
@@ -104,6 +112,9 @@ static int hf_rlc_sufi_sn_mrw = -1;
 static int hf_rlc_sufi_poll_sn = -1;
 static int hf_rlc_header_only = -1;
 static int hf_rlc_channel = -1;
+static int hf_rlc_channel_rbid = -1;
+static int hf_rlc_channel_dir = -1;
+static int hf_rlc_channel_ueid = -1;
 
 /* subtrees */
 static int ett_rlc = -1;
@@ -113,6 +124,7 @@ static int ett_rlc_sdu = -1;
 static int ett_rlc_sufi = -1;
 static int ett_rlc_bitmap = -1;
 static int ett_rlc_rlist = -1;
+static int ett_rlc_channel = -1;
 
 static dissector_handle_t ip_handle;
 static dissector_handle_t rrc_handle;
@@ -120,6 +132,7 @@ static dissector_handle_t bmc_handle;
 
 enum rlc_channel_type {
 	RLC_PCCH,
+	RLC_BCCH,
 	RLC_UL_CCCH,
 	RLC_DL_CCCH,
 	RLC_UL_DCCH,
@@ -874,7 +887,7 @@ add_fragment(enum rlc_mode mode, tvbuff_t *tvb, packet_info *pinfo,
 	}
 	rlc_frag_assign(&frag_lookup, mode, pinfo, seq, num_li);
 	#if RLC_ADD_FRAGMENT_DEBUG_PRINT
-		g_print("packet: %d, channel (%d %d %d %d %d %d %d %d %d) seq: %u, num_li: %u, offset: %u, \n", pinfo->fd->num, ch_lookup.cid, ch_lookup.dir, ch_lookup.li_size, ch_lookup.link, ch_lookup.mode, ch_lookup.rbid, ch_lookup.urnti, ch_lookup.vci, ch_lookup.vpi, seq, num_li, offset);
+		g_print("packet: %d, channel (%d %d %d) seq: %u, num_li: %u, offset: %u, \n", pinfo->fd->num, ch_lookup.dir, ch_lookup.rbid, ch_lookup.urnti, seq, num_li, offset);
 	#endif
 	/* look for an already assembled SDU */
 	if (g_hash_table_lookup_extended(reassembled_table, &frag_lookup, &orig_key, &value)) {
@@ -922,13 +935,18 @@ add_fragment(enum rlc_mode mode, tvbuff_t *tvb, packet_info *pinfo,
 					expert_add_info_format(pinfo, NULL, PI_REASSEMBLE, PI_ERROR, "Did not perform reassembly because of previous unfinished sequence.");
 				}
 			} else if (endlist->list) {
-				gint16 end = GPOINTER_TO_INT(endlist->list->data);
-				if (end >= 0 && end < 4096 && frags[end]) {
-					proto_tree_add_text(tree, tvb, 0, 0, "Did not perform reassembly because of unfinished sequence, found lingering endpoint (%d [packet %d]).", end, frags[end]->frame_num);
+				if (fail) {
+					proto_tree_add_text(tree, tvb, 0, 0, "Did not perform reassembly because fail flag was set previously.");
+					expert_add_info_format(pinfo, NULL, PI_REASSEMBLE, PI_ERROR, "Did not perform reassembly because fail flag was set previously.");
 				} else {
-					proto_tree_add_text(tree, tvb, 0, 0, "Did not perform reassembly because of unfinished sequence, found lingering endpoint (%d [could not determine packet]).", end);
+					gint16 end = GPOINTER_TO_INT(endlist->list->data);
+					if (end >= 0 && end < 4096 && frags[end]) {
+						proto_tree_add_text(tree, tvb, 0, 0, "Did not perform reassembly because of unfinished sequence, found lingering endpoint (%d [packet %d]).", end, frags[end]->frame_num);
+					} else {
+						proto_tree_add_text(tree, tvb, 0, 0, "Did not perform reassembly because of unfinished sequence, found lingering endpoint (%d [could not determine packet]).", end);
+					}
+					expert_add_info_format(pinfo, NULL, PI_REASSEMBLE, PI_ERROR, "Lingering endpoint.");
 				}
-				expert_add_info_format(pinfo, NULL, PI_REASSEMBLE, PI_ERROR, "Lingering endpoint.");
 			} else {
 				expert_add_info_format(pinfo, NULL, PI_REASSEMBLE, PI_ERROR, "Unknown error.");
 			}
@@ -1003,7 +1021,7 @@ add_fragment(enum rlc_mode mode, tvbuff_t *tvb, packet_info *pinfo,
 			if (frags[start] != NULL) {
 				endlist->list->data = GINT_TO_POINTER(start-1);
 			}
-			/* NOTE: frags[start] is se_alloced and will remain until file closes, we would want to free it here. */
+			/* NOTE: frags[start] is se_alloced and will remain until file closes, we would want to free it here maybe. */
 			return NULL;
 		}
 
@@ -1172,6 +1190,9 @@ rlc_call_subdissector(enum rlc_channel_type channel, tvbuff_t *tvb,
 		case RLC_PCCH:
 			msgtype = RRC_MESSAGE_TYPE_PCCH;
 			break;
+		case RLC_BCCH:
+			msgtype = RRC_MESSAGE_TYPE_BCCH_FACH;
+			break;
 		case RLC_PS_DTCH:
 			msgtype = RRC_MESSAGE_TYPE_INVALID;
 			/* assume transparent PDCP for now */
@@ -1240,7 +1261,7 @@ rlc_um_reassemble(tvbuff_t *tvb, guint8 offs, packet_info *pinfo, proto_tree *tr
 				}
 				if (global_rlc_perform_reassemby) {
 					add_fragment(RLC_UM, tvb, pinfo, li[i].tree, offs, seq, i, length, TRUE);
-					next_tvb = get_reassembled_data(RLC_UM, tvb, pinfo, li[i].tree, seq, i);
+					next_tvb = get_reassembled_data(RLC_UM, tvb, pinfo, tree, seq, i);
 				}
 				offs += length;
 			}
@@ -1254,7 +1275,7 @@ rlc_um_reassemble(tvbuff_t *tvb, guint8 offs, packet_info *pinfo, proto_tree *tr
 			}
 			if (global_rlc_perform_reassemby) {
 				add_fragment(RLC_UM, tvb, pinfo, li[i].tree, offs, seq, i, li[i].len, TRUE);
-				next_tvb = get_reassembled_data(RLC_UM, tvb, pinfo, li[i].tree, seq, i);
+				next_tvb = get_reassembled_data(RLC_UM, tvb, pinfo, tree, seq, i);
 			}
 		}
 		if (next_tvb) {
@@ -1436,12 +1457,18 @@ rlc_decode_li(enum rlc_mode mode, tvbuff_t *tvb, packet_info *pinfo, proto_tree 
 static void add_channel_info(packet_info * pinfo, proto_tree * tree, fp_info * fpinf, rlc_info * rlcinf)
 {
 	proto_item * item;
+	proto_tree * channel_tree;
 
-	if (pinfo->p2p_dir != LINK_DIR_UNKNOWN) {
-		item = proto_tree_add_item(tree, hf_rlc_channel, NULL, 0, 0, ENC_NA);
-		proto_item_append_text(item, " (rbid: %u, dir: %u, uid: %u)", rlcinf->rbid[fpinf->cur_tb], pinfo->p2p_dir, rlcinf->urnti[fpinf->cur_tb]);
-		PROTO_ITEM_SET_GENERATED(item);
-	}
+	item = proto_tree_add_item(tree, hf_rlc_channel, NULL, 0, 0, ENC_NA);
+	channel_tree = proto_item_add_subtree(item, ett_rlc_channel);
+	proto_item_append_text(item, " (rbid: %u, dir: %u, uid: %u)", rlcinf->rbid[fpinf->cur_tb], pinfo->p2p_dir, rlcinf->urnti[fpinf->cur_tb]);
+	PROTO_ITEM_SET_GENERATED(item);
+	item = proto_tree_add_uint(channel_tree, hf_rlc_channel_rbid, NULL, 0, 0, rlcinf->rbid[fpinf->cur_tb]);
+	PROTO_ITEM_SET_GENERATED(item);
+	item = proto_tree_add_uint(channel_tree, hf_rlc_channel_dir, NULL, 0, 0, pinfo->p2p_dir);
+	PROTO_ITEM_SET_GENERATED(item);
+	item = proto_tree_add_uint(channel_tree, hf_rlc_channel_ueid, NULL, 0, 0, rlcinf->urnti[fpinf->cur_tb]);
+	PROTO_ITEM_SET_GENERATED(item);
 }
 
 static void
@@ -1488,10 +1515,14 @@ dissect_rlc_um(enum rlc_channel_type channel, tvbuff_t *tvb, packet_info *pinfo,
 		return;
 	}
 
-	if (rlcinf->li_size[pos] == RLC_LI_VARIABLE) {
-		li_is_on_2_bytes = (tvb_length(tvb) > 125) ? TRUE : FALSE;
-	} else {
-		li_is_on_2_bytes = (rlcinf->li_size[pos] == RLC_LI_15BITS) ? TRUE : FALSE;
+	if (global_rlc_li_size == RLC_LI_UPPERLAYER) {
+		if (rlcinf->li_size[pos] == RLC_LI_VARIABLE) {
+			li_is_on_2_bytes = (tvb_length(tvb) > 126) ? TRUE : FALSE;
+		} else {
+			li_is_on_2_bytes = (rlcinf->li_size[pos] == RLC_LI_15BITS) ? TRUE : FALSE;
+		}
+	} else { /* Overrid rlcinf configuration with preference. */
+		li_is_on_2_bytes = (global_rlc_li_size == RLC_LI_15BITS) ? TRUE : FALSE;
 	}
 
 	num_li = rlc_decode_li(RLC_UM, tvb, pinfo, tree, li, MAX_LI, li_is_on_2_bytes);
@@ -1790,8 +1821,14 @@ rlc_am_reassemble(tvbuff_t *tvb, guint8 offs, packet_info *pinfo,
 			piggyback = TRUE;
 		} else if ((!li_is_on_2_bytes && (li[i].li == 0x7f)) || (li[i].li == 0x7fff)) {
 			/* padding, must be last LI */
-			if (tree && tvb_length_remaining(tvb, offs) > 0) {
-				proto_tree_add_item(tree, hf_rlc_pad, tvb, offs, -1, ENC_NA);
+			if (tvb_length_remaining(tvb, offs) > 0) {
+				if (tree) {
+					proto_tree_add_item(tree, hf_rlc_pad, tvb, offs, -1, ENC_NA);
+				}
+				if (i == 0) {
+					/* Insert empty RLC frag so RLC doesn't miss this seq number. */
+					add_fragment(RLC_AM, tvb, pinfo, li[i].tree, offs, seq, i, 0, TRUE);
+				}
 			}
 			offs += tvb_length_remaining(tvb, offs);
 		} else {
@@ -1909,10 +1946,15 @@ dissect_rlc_am(enum rlc_channel_type channel, tvbuff_t *tvb, packet_info *pinfo,
 		col_append_str(pinfo->cinfo, COL_INFO, "[Ciphered Data]");
 		return;
 	}
-	if (rlcinf->li_size[pos] == RLC_LI_VARIABLE) {
-		li_is_on_2_bytes = (tvb_length(tvb) > 126) ? TRUE : FALSE;
-	} else {
-		li_is_on_2_bytes = (rlcinf->li_size[pos] == RLC_LI_15BITS) ? TRUE : FALSE;
+
+	if (global_rlc_li_size == RLC_LI_UPPERLAYER) {
+		if (rlcinf->li_size[pos] == RLC_LI_VARIABLE) {
+			li_is_on_2_bytes = (tvb_length(tvb) > 126) ? TRUE : FALSE;
+		} else {
+			li_is_on_2_bytes = (rlcinf->li_size[pos] == RLC_LI_15BITS) ? TRUE : FALSE;
+		}
+	} else { /* Overrid rlcinf configuration with preference. */
+		li_is_on_2_bytes = (global_rlc_li_size == RLC_LI_15BITS) ? TRUE : FALSE;
 	}
 
 	num_li = rlc_decode_li(RLC_AM, tvb, pinfo, tree, li, MAX_LI, li_is_on_2_bytes);
@@ -1969,6 +2011,27 @@ dissect_rlc_pcch(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 		proto_item_append_text(ti, " TM (PCCH)");
 	}
 	dissect_rlc_tm(RLC_PCCH, tvb, pinfo, tree, subtree);
+}
+
+static void
+dissect_rlc_bcch(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+	fp_info    *fpi;
+	proto_item *ti      = NULL;
+	proto_tree *subtree = NULL;
+
+	col_set_str(pinfo->cinfo, COL_PROTOCOL, "RLC");
+	col_clear(pinfo->cinfo, COL_INFO);
+
+	fpi = p_get_proto_data(pinfo->fd, proto_fp);
+	if (!fpi) return; /* dissection failure */
+
+	if (tree) {
+		ti = proto_tree_add_item(tree, proto_rlc, tvb, 0, -1, ENC_NA);
+		subtree = proto_item_add_subtree(ti, ett_rlc);
+	}
+	proto_item_append_text(ti, " TM (BCCH)");
+	dissect_rlc_tm(RLC_BCCH, tvb, pinfo, tree, subtree);
 }
 
 static void
@@ -2463,6 +2526,18 @@ proto_register_rlc(void)
 		{ &hf_rlc_channel,
 		  { "Channel", "rlc.channel",
 		    FT_NONE, BASE_NONE, NULL, 0, NULL, HFILL }
+		},
+		{ &hf_rlc_channel_rbid,
+		  { "Radio Bearer ID", "rlc.channel.rbid",
+		    FT_UINT8, BASE_DEC, NULL, 0, NULL, HFILL }
+		},
+		{ &hf_rlc_channel_dir,
+		  { "Direction", "rlc.channel.dir",
+		    FT_UINT8, BASE_DEC, NULL, 0, NULL, HFILL }
+		},
+		{ &hf_rlc_channel_ueid,
+		  { "User Equipment ID", "rlc.channel.ueid",
+		    FT_UINT32, BASE_DEC, NULL, 0, NULL, HFILL }
 		}
  	};
 	static gint *ett[] = {
@@ -2473,8 +2548,10 @@ proto_register_rlc(void)
 		&ett_rlc_sufi,
 		&ett_rlc_bitmap,
 		&ett_rlc_rlist,
+		&ett_rlc_channel
 	};
 	proto_rlc = proto_register_protocol("Radio Link Control", "RLC", "rlc");
+	register_dissector("rlc.bcch",        dissect_rlc_bcch,        proto_rlc);
 	register_dissector("rlc.pcch",        dissect_rlc_pcch,        proto_rlc);
 	register_dissector("rlc.ccch",        dissect_rlc_ccch,        proto_rlc);
 	register_dissector("rlc.ctch",        dissect_rlc_ctch,        proto_rlc);
@@ -2510,6 +2587,11 @@ proto_register_rlc(void)
 		"Ciphered data",
 		"When enabled, rlc will assume all data is ciphered and won't process it ",
 		&global_rlc_ciphered);
+
+	prefs_register_enum_preference(rlc_module, "li_size",
+		"LI size",
+		"LI size in bits, either 7 or 15 bit",
+		&global_rlc_li_size, li_size_enumvals, FALSE);
 
 	register_init_routine(fragment_table_init);
 }

epan/dissectors/packet-rlc.h
@@ -38,7 +38,8 @@ enum rlc_mode {
 enum rlc_li_size {
 	RLC_LI_VARIABLE,
 	RLC_LI_7BITS,
-	RLC_LI_15BITS
+	RLC_LI_15BITS,
+	RLC_LI_UPPERLAYER /* Let upper layers decide LI-size rather than preference. */
 };
 
 #define MAX_RLC_CHANS 64

epan/dissectors/packet-umts_fp.c
@@ -47,7 +47,7 @@
  *
  * TODO:
  *  - IUR interface-specific formats
- *  - verify header & payload CRCs
+ *  - verify header CRCs
  *  - do CRC verification before further parsing
  *    - Set the logical channel properly for non multiplexed, channels
  *     for channels that doesn't have the C/T flag! This should be based
@@ -149,6 +149,7 @@ static int hf_fp_hsdsch_new_ie_flags = -1;
 static int hf_fp_hsdsch_new_ie_flag[8] = {-1, -1, -1, -1, -1, -1, -1, -1};
 static int hf_fp_hsdsch_drt = -1;
 static int hf_fp_hsdsch_entity = -1;
+static int hf_fp_hsdsch_physical_layer_category = -1;
 static int hf_fp_timing_advance = -1;
 static int hf_fp_num_of_pdu = -1;
 static int hf_fp_mac_d_pdu_len = -1;
@@ -209,6 +210,7 @@ static int ett_fp_hsdsch_new_ie_flags = -1;
 static int ett_fp_rach_new_ie_flags = -1;
 static int ett_fp_hsdsch_pdu_block_header = -1;
 
+static dissector_handle_t rlc_bcch_handle;
 static dissector_handle_t mac_fdd_dch_handle;
 static dissector_handle_t mac_fdd_rach_handle;
 static dissector_handle_t mac_fdd_fach_handle;
@@ -2902,7 +2904,13 @@ dissect_e_dch_t2_or_common_channel_info(tvbuff_t *tvb, packet_info *pinfo, proto
             }
             p_add_proto_data(pinfo->fd, proto_umts_mac, mac_is_info);
             call_dissector(mac_fdd_edch_type2_handle, tvb_new_subset(tvb, offset, -1, -1), pinfo, top_level_tree);
-            offset += length + 1; /* XXX TODO this is wrong if TSN is 14 bits (then it is +2) */
+
+            /* get_mac_tsn_size in packet-umts_mac.h, gets the global_mac_tsn_size preference in umts_mac.c */
+            if (get_mac_tsn_size() == MAC_TSN_14BITS) {
+                offset += length + 2; /* Plus 2 bytes for TSN 14 bits and SS 2 bit. */
+            } else {
+                offset += length + 1; /* Plus 1 byte for TSN 6 bits and SS 2 bit. */
+            }
         }
     }
 
@@ -3019,7 +3027,7 @@ dissect_hsdsch_channel_info(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
             rlcinf->urnti[i] = p_fp_info->channel;
             rlcinf->li_size[i] = RLC_LI_7BITS;
             rlcinf->deciphered[i] = FALSE;
-            rlcinf->rbid[i] = 16;
+            rlcinf->rbid[i] = macinf->lchid[i];
         }
 
 
@@ -3300,7 +3308,7 @@ dissect_hsdsch_type_2_channel_info(tvbuff_t *tvb, packet_info *pinfo, proto_tree
 *
 * This will dissect hsdsch common channels of type 2, so this is
 * very similar to regular type two (ehs) the diffrence being how
-* the configuration is done.
+* the configuration is done. NOTE: VERY EXPERIMENTAL.
 *
 * @param tvb
 * @param pinfo packet info.
@@ -3339,6 +3347,9 @@ void dissect_hsdsch_common_channel_info(tvbuff_t *tvb, packet_info *pinfo, proto
         guint64 lchid[MAX_PDU_BLOCKS];
         guint64 pdu_length[MAX_PDU_BLOCKS];
         guint64 no_of_pdus[MAX_PDU_BLOCKS];
+        guint header_length = 0;
+        guint8 newieflags = 0;
+        guint8 hsdsch_physical_layer_category = 0;
 
         umts_mac_info *macinf;
         rlc_info *rlcinf;
@@ -3459,7 +3470,9 @@ void dissect_hsdsch_common_channel_info(tvbuff_t *tvb, packet_info *pinfo, proto
             }
         }
 
-
+        if (header_length == 0) {
+            header_length = offset;
+        }
         /**********************************************/
         /* Optional fields indicated by earlier flags */
         if (drt_present) {
@@ -3481,43 +3494,55 @@ void dissect_hsdsch_common_channel_info(tvbuff_t *tvb, packet_info *pinfo, proto
         /********************************************************************/
         /* Now read the MAC-d/c PDUs for each block using info from headers */
         for (n=0; n < number_of_pdu_blocks; n++) {
+            tvbuff_t * next_tvb;
             for(j=0;j<no_of_pdus[n];j++){
-
-                    if(lchid[n] == 0xF){    /*If all bits are set, then this is BCCH or PCCH according to: 25.435 paragraph: 6.2.7.31*/
-                        /*BCCH, then we have a MAC-ehs header*/
-                        /*PCCH then we don't have any MAC-c header */
-                        macinf->content[j] = MAC_BCCH;
-                        macinf->lchid[j] = MAC_N_A;
-                        continue;
-                    }
-                /*Configure (signal to lower layers) the PDU!*/
-                    macinf->content[j] = lchId_type_table[lchid[n]+1];/*hsdsch_macdflow_id_mac_content_map[p_fp_info->hsdsch_macflowd_id];*/ /*MAC_CONTENT_PS_DTCH;*/
-                    macinf->lchid[j] = (guint8)lchid[n]+1;    /*Add 1 since C/T is zero indexed? ie C/T =0 => L-CHID = 1*/
-
+                /* If all bits are set, then this is BCCH or PCCH according to: 25.435 paragraph: 6.2.7.31 */
+                if(lchid[n] == 0xF) {
+                    /* In the very few test cases I've seen, this seems to be
+                     * BCCH with transparent MAC layer. Therefore skip right to
+                     * rlc_bcch and hope for the best. */
+                    next_tvb = tvb_new_subset(tvb, offset, (gint)pdu_length[n], (gint)pdu_length[n]);
+                    call_dissector(rlc_bcch_handle, next_tvb, pinfo, top_level_tree);
+                    offset += (gint)pdu_length[n];
+                } else { /* Else go for CCCH UM, this seems to work. */
+                    p_fp_info->hsdsch_entity = ehs; /* HSDSCH type 2 */
+                    /* TODO: use cur_tb or subnum everywhere. */
+                    p_fp_info->cur_tb = j; /* set cur_tb for MAC */
+                    pinfo->fd->subnum = j; /* set subframe number for RRC */
+                    macinf->content[j] = MAC_CONTENT_CCCH;
+                    macinf->lchid[j] = (guint8)lchid[n]+1; /*Add 1 since it is zero indexed? */
                     macinf->macdflow_id[j] = p_fp_info->hsdsch_macflowd_id;
-
-                    /*Figure out RLC_MODE based on MACd-flow-ID, basically MACd-flow-ID = 0 then its SRB0 == UM else AM*/
-
-                    rlcinf->mode[j] = RLC_TM;/*lchId_rlc_map[lchid[n]+1];*//*hsdsch_macdflow_id_rlc_map[p_fp_info->hsdsch_macflowd_id];*/
-
-                    macinf->ctmux[n] = TRUE;    /*This is handled already in FP*/
+                    macinf->ctmux[j] = FALSE;
                     rlcinf->li_size[j] = RLC_LI_7BITS;
                     /*rlcinf->ciphered[j] = FALSE;*/
                     rlcinf->deciphered[j] = FALSE;
                     rlcinf->rbid[j] = (guint8)lchid[n]+1;
-                    rlcinf->urnti[j] = p_fp_info->channel;    /*We need to fake urnti*/
+                    rlcinf->urnti[j] = p_fp_info->channel; /*We need to fake urnti*/
 
+                    next_tvb = tvb_new_subset(tvb, offset, (gint)pdu_length[n], (gint)pdu_length[n]);
+                    call_dissector(mac_fdd_hsdsch_handle, next_tvb, pinfo, top_level_tree);
+
+                    offset += (gint)pdu_length[n];
+                }
             }
-            /* Add PDU block header subtree */
-            offset = dissect_macd_pdu_data_type_2(tvb, pinfo, tree, offset,
-                                                  (guint16)pdu_length[n],
-                                                  (guint16)no_of_pdus[n],p_fp_info);
+        }
 
-
+        /* New IE Flags */
+        newieflags = tvb_get_guint8(tvb, offset);
+        /* If newieflags == 0000 0010 then this indicates that there is a
+         * HS-DSCH physical layer category and no other New IE flags. */
+        if (newieflags == 2) {
+            /* HS-DSCH physical layer category presence bit. */
+            proto_tree_add_uint(tree, hf_fp_hsdsch_new_ie_flag[6], tvb, offset, 1, newieflags);
+            offset++;
+            /* HS-DSCH physical layer category. */
+            hsdsch_physical_layer_category = tvb_get_bits8(tvb, offset*8, 6);
+            proto_tree_add_bits_item(tree, hf_fp_hsdsch_physical_layer_category, tvb, offset*8, 6, ENC_BIG_ENDIAN);
+            offset++;
         }
 
         /* Spare Extension and Payload CRC */
-        dissect_spare_extension_and_crc(tvb, pinfo, tree, 1, offset,0);
+        dissect_spare_extension_and_crc(tvb, pinfo, tree, 1, offset, header_length);
     }
 
 }
@@ -3574,26 +3599,25 @@ heur_dissect_fp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     dissect_fp(tvb, pinfo, tree);
     return TRUE;
 }
-static guint8 fakes =5; /*[] ={1,5,8};*/
-static guint8 fake_map[31];
 
+static guint8 fakes = 5; /*[] ={1,5,8};*/
+static guint8 fake_map[31];
 /*
- * TODO:
- * This need to be fixed!
- * Basically you would want the actual RRC messages, that sooner or later maps transport channel id's to logical id's
- * to set the proper logical channel, but for now we make syntethic ones.
+ * TODO: This need to be fixed!
+ * Basically you would want the actual RRC messages, that sooner or later maps
+ * transport channel id's to logical id's or RAB IDs
+ * to set the proper logical channel/RAB ID, but for now we make syntethic ones.
  * */
 static guint8
-make_fake_lchid(packet_info *pinfo _U_, gint trchld){
-
-        if( fake_map[trchld] == 0){
-            fake_map[trchld] = fakes;
-            fakes++;
-        }
-
-        return fake_map[trchld];
-
+make_fake_lchid(packet_info *pinfo _U_, gint trchld)
+{
+    if( fake_map[trchld] == 0){
+        fake_map[trchld] = fakes;
+        fakes++;
     }
+    return fake_map[trchld];
+}
+
 /*
  * july 2012:
  * Alot of configuration has been move into the actual dissecting functions
@@ -3809,6 +3833,7 @@ fp_set_per_packet_inf_from_conv(umts_fp_conversation_info_t *p_conv_data,
                                 rlcinf->mode[j+chan] = lchId_rlc_map[c_t+1];    /*Based RLC mode on logical channel id*/
                             }
                         }else{
+                            fake_lchid = make_fake_lchid(pinfo,p_conv_data->dchs_in_flow_list[chan]);
                             macinf->ctmux[j+chan] = FALSE;/*Set TRUE if this channel is multiplexed (ie. C/T flag exists)*/
                             /*macinf->content[j+chan] = MAC_CONTENT_CS_DTCH;*/
                             macinf->content[j+chan] = lchId_type_table[fake_lchid];
@@ -4156,12 +4181,9 @@ dissect_fp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             }
             break;
         case CHANNEL_HSDSCH_COMMON:
-
-            if(FALSE)
-            dissect_hsdsch_common_channel_info(tvb,pinfo, tree,offset, p_fp_info);
-
-            expert_add_info_format(pinfo, NULL, PI_DEBUG, PI_ERROR, "HSDSCH COMMON - Not implemented!");
-
+            expert_add_info_format(pinfo, NULL, PI_DEBUG, PI_WARN, "HSDSCH COMMON - Experimental support!");
+            /*if(FALSE)*/
+            dissect_hsdsch_common_channel_info(tvb,pinfo, fp_tree, offset, p_fp_info);
             break;
         case CHANNEL_HSDSCH_COMMON_T3:
          expert_add_info_format(pinfo, NULL, PI_DEBUG, PI_ERROR, "HSDSCH COMMON T3 - Not implemeneted!");
@@ -4781,7 +4803,7 @@ void proto_register_fp(void)
               }
             },
             { &hf_fp_hsdsch_new_ie_flag[6],
-              { "New IE present",
+              { "HS-DSCH physical layer category present",
                 "fp.hsdsch.new-ie-flag", FT_UINT8, BASE_DEC, 0, 0x02,
                 NULL, HFILL
               }
@@ -5106,7 +5128,12 @@ void proto_register_fp(void)
                 NULL, HFILL
               }
             },
-
+            { &hf_fp_hsdsch_physical_layer_category,
+              { "HS-DSCH physical layer category",
+                "fp.hsdsch.physical_layer_category", FT_UINT8, BASE_DEC, NULL, 0x0,
+                NULL, HFILL
+              }
+            }
         };
 
 
@@ -5161,6 +5188,7 @@ void proto_register_fp(void)
 
 void proto_reg_handoff_fp(void)
 {
+    rlc_bcch_handle = find_dissector("rlc.bcch");
     mac_fdd_rach_handle   = find_dissector("mac.fdd.rach");
     mac_fdd_fach_handle   = find_dissector("mac.fdd.fach");
     mac_fdd_pch_handle    = find_dissector("mac.fdd.pch");

commit 3d3f47e0faa492f62dbfcb768102c625c3669482

Various fixes: - in UM mode, LI size boundary is at 125 bytes (regression introduced in r44292) - define RLC_LI_UPPERLAYER outside of rlc_li_size enum as it is confusing: when set in preferences it means that the dissector will use the variable, 7 bits or 15 bits LI as indicated in the rlc_info struct (so using one of the rlc_li_size enum value); it is not a new LI value - add value_string array for direction - add BCCH channel to UDP framing format - initialize p2p_dir when using UDP framing format
git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@44318 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-rlc.c
@@ -62,13 +62,14 @@ static gboolean global_rlc_headers_expected = FALSE;
 /* Heuristic dissection */
 static gboolean global_rlc_heur = FALSE;
 
-/* Preferences to expect ciphered data*/
+/* Preferences to expect ciphered data */
 static gboolean global_rlc_ciphered = FALSE;
 
 /* Stop trying to do reassembly if this is true. */
 static gboolean fail = FALSE;
 
 /* LI size preference */
+#define RLC_LI_UPPERLAYER 255 /* LI-size comes from rlc_info struct rather than preference */
 static gint global_rlc_li_size = RLC_LI_UPPERLAYER;
 static const enum_val_t li_size_enumvals[] = {
 	{"7 bits", "7 bits", RLC_LI_7BITS},
@@ -140,6 +141,12 @@ enum rlc_channel_type {
 	RLC_PS_DTCH,
 	RLC_DL_CTCH,
 	RLC_UNKNOWN_CH
+};
+
+static const value_string rlc_dir_vals[] = {
+	{ P2P_DIR_UL, "Uplink" },
+	{ P2P_DIR_DL, "Downlink" },
+	{ 0, NULL }
 };
 
 static const true_false_string rlc_header_only_val = {
@@ -1461,7 +1468,8 @@ static void add_channel_info(packet_info * pinfo, proto_tree * tree, fp_info * f
 
 	item = proto_tree_add_item(tree, hf_rlc_channel, NULL, 0, 0, ENC_NA);
 	channel_tree = proto_item_add_subtree(item, ett_rlc_channel);
-	proto_item_append_text(item, " (rbid: %u, dir: %u, uid: %u)", rlcinf->rbid[fpinf->cur_tb], pinfo->p2p_dir, rlcinf->urnti[fpinf->cur_tb]);
+	proto_item_append_text(item, " (rbid: %u, dir: %s, uid: %u)", rlcinf->rbid[fpinf->cur_tb], 
+	                       val_to_str_const(pinfo->p2p_dir, rlc_dir_vals, "Unknown"), rlcinf->urnti[fpinf->cur_tb]);
 	PROTO_ITEM_SET_GENERATED(item);
 	item = proto_tree_add_uint(channel_tree, hf_rlc_channel_rbid, NULL, 0, 0, rlcinf->rbid[fpinf->cur_tb]);
 	PROTO_ITEM_SET_GENERATED(item);
@@ -1517,11 +1525,11 @@ dissect_rlc_um(enum rlc_channel_type channel, tvbuff_t *tvb, packet_info *pinfo,
 
 	if (global_rlc_li_size == RLC_LI_UPPERLAYER) {
 		if (rlcinf->li_size[pos] == RLC_LI_VARIABLE) {
-			li_is_on_2_bytes = (tvb_length(tvb) > 126) ? TRUE : FALSE;
+			li_is_on_2_bytes = (tvb_length(tvb) > 125) ? TRUE : FALSE;
 		} else {
 			li_is_on_2_bytes = (rlcinf->li_size[pos] == RLC_LI_15BITS) ? TRUE : FALSE;
 		}
-	} else { /* Overrid rlcinf configuration with preference. */
+	} else { /* Override rlcinf configuration with preference. */
 		li_is_on_2_bytes = (global_rlc_li_size == RLC_LI_15BITS) ? TRUE : FALSE;
 	}
 
@@ -1953,7 +1961,7 @@ dissect_rlc_am(enum rlc_channel_type channel, tvbuff_t *tvb, packet_info *pinfo,
 		} else {
 			li_is_on_2_bytes = (rlcinf->li_size[pos] == RLC_LI_15BITS) ? TRUE : FALSE;
 		}
-	} else { /* Overrid rlcinf configuration with preference. */
+	} else { /* Override rlcinf configuration with preference. */
 		li_is_on_2_bytes = (global_rlc_li_size == RLC_LI_15BITS) ? TRUE : FALSE;
 	}
 
@@ -2282,7 +2290,13 @@ dissect_rlc_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 				rlcModePresent = TRUE;
 				break;
 			case RLC_DIRECTION_TAG:
-				fpi->is_uplink = (tvb_get_guint8(tvb, offset) == DIRECTION_UPLINK) ? TRUE : FALSE;
+				if (tvb_get_guint8(tvb, offset) == DIRECTION_UPLINK) {
+					fpi->is_uplink = TRUE;
+					pinfo->p2p_dir = P2P_DIR_UL;
+				} else {
+					fpi->is_uplink = FALSE;
+					pinfo->p2p_dir = P2P_DIR_DL;
+				}
 				offset++;
 				break;
 			case RLC_URNTI_TAG:
@@ -2360,6 +2374,9 @@ dissect_rlc_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			break;
 		case UMTS_CHANNEL_TYPE_CTCH:
 			dissect_rlc_ctch(rlc_tvb, pinfo, tree);
+			break;
+		case UMTS_CHANNEL_TYPE_BCCH:
+			dissect_rlc_bcch(rlc_tvb, pinfo, tree);
 			break;
 
 		default:
@@ -2533,7 +2550,7 @@ proto_register_rlc(void)
 		},
 		{ &hf_rlc_channel_dir,
 		  { "Direction", "rlc.channel.dir",
-		    FT_UINT8, BASE_DEC, NULL, 0, NULL, HFILL }
+		    FT_UINT8, BASE_DEC, VALS(rlc_dir_vals), 0, NULL, HFILL }
 		},
 		{ &hf_rlc_channel_ueid,
 		  { "User Equipment ID", "rlc.channel.ueid",

epan/dissectors/packet-rlc.h
@@ -38,8 +38,7 @@ enum rlc_mode {
 enum rlc_li_size {
 	RLC_LI_VARIABLE,
 	RLC_LI_7BITS,
-	RLC_LI_15BITS,
-	RLC_LI_UPPERLAYER /* Let upper layers decide LI-size rather than preference. */
+	RLC_LI_15BITS
 };
 
 #define MAX_RLC_CHANS 64
@@ -86,6 +85,7 @@ typedef struct rlc_info
 #define UMTS_CHANNEL_TYPE_DCCH 3
 #define UMTS_CHANNEL_TYPE_PS_DTCH 4
 #define UMTS_CHANNEL_TYPE_CTCH 5
+#define UMTS_CHANNEL_TYPE_BCCH 6
 
 #define RLC_CHANNEL_TYPE_TAG    0x02
 /* 1 byte */