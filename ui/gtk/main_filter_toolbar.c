AnkitKejriwal/wireshark

commit b756de38ce8ebed6346207248a5ddcd1362e0d29

Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6892 :
Sort the recent filters list by "most recently used" (that is, put the most
recently used filter at the head of the list).


git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@42439 f5534014-38df-0310-8fa8-9805f1628bb7

ui/gtk/main_filter_toolbar.c
@@ -300,7 +300,7 @@ dfilter_entry_match(GtkWidget *filter_cm, char *s, int *index)
         *index = i;
         return FALSE;
     }
-    do{
+    do {
         i++;
         gtk_tree_model_get_value (model, &iter, 0, &value);
         filter_str = g_value_get_string (&value);
@@ -345,14 +345,14 @@ dfilter_recent_combo_write_all(FILE *rf) {
 
     if (!gtk_tree_model_get_iter_first (model, &iter))
         return;
-    do{
+    do {
         gtk_tree_model_get_value (model, &iter, 0, &value);
         filter_str = g_value_get_string (&value);
         if(filter_str)
             fprintf (rf, RECENT_KEY_DISPLAY_FILTER ": %s\n", filter_str);
         g_value_unset (&value);
 
-    }while (gtk_tree_model_iter_next (model, &iter)&& (max_count++ < prefs.gui_recent_df_entries_max));
+    } while (gtk_tree_model_iter_next (model, &iter)&& (max_count++ < prefs.gui_recent_df_entries_max));
 
 }
 
@@ -397,10 +397,20 @@ main_filter_packets(capture_file *cf, const gchar *dftext, gboolean force)
     if (cf_status == CF_OK && strlen(s) > 0) {
         int index;
 
-        if(!dfilter_entry_match(filter_cm,s, &index)) {
+        if(!dfilter_entry_match(filter_cm,s, &index) || index != 0) {
+
+            /* If the filter is already there but not the first entry, remove it */
+            if (index > 0) {
+                gtk_combo_box_text_remove(GTK_COMBO_BOX_TEXT(filter_cm), index);
+                index--;
+            }
+
+            /* Add the filter (at the head of the list) */
             gtk_combo_box_text_prepend_text(GTK_COMBO_BOX_TEXT(filter_cm), s);
             index++;
         }
+
+        /* If we have too many entries, remove some */
         while ((guint)index >= prefs.gui_recent_df_entries_max) {
             gtk_combo_box_text_remove(GTK_COMBO_BOX_TEXT(filter_cm), index);
             index--;

commit 268130f7452f57a73a697b05cf9bcfe495fbb2e4

Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=7731 :
Allow the configured number of dfilters to be added to the drop-down list
(instead of limiting the list--on a freshly installed system with no recent
dfilters--to just 2).  Regression caused by r42439.


git-svn-id: svn+ssh://svn.wireshark.org/svn/wireshark/trunk@45012 f5534014-38df-0310-8fa8-9805f1628bb7

ui/gtk/main_filter_toolbar.c
@@ -314,7 +314,7 @@ dfilter_entry_match(GtkWidget *filter_cm, char *s, int *index)
         g_value_unset (&value);
     } while (gtk_tree_model_iter_next (model, &iter));
 
-    *index = i;
+    *index = -1;
     return FALSE;
 }
 
@@ -397,10 +397,10 @@ main_filter_packets(capture_file *cf, const gchar *dftext, gboolean force)
     if (cf_status == CF_OK && strlen(s) > 0) {
         int index;
 
-        if(!dfilter_entry_match(filter_cm,s, &index) || index != 0) {
+        if(!dfilter_entry_match(filter_cm,s, &index) || index > -1) {
 
             /* If the filter is already there but not the first entry, remove it */
-            if (index > 0) {
+            if (index > -1) {
                 gtk_combo_box_text_remove(GTK_COMBO_BOX_TEXT(filter_cm), index);
                 index--;
             }